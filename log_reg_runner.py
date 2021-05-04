2792621cd34c7a1a439362894396a02cc8743825
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2021-05-04 10:51:21 -0400

    Added comments

--- log_reg_runner.py
+++ log_reg_runner.py
@@ -1,6 +1,3 @@
-# Don't change these imports. Note that the last two are the
-# class implementations that you will implement in
-# T2_P3_LogisticRegression.py and T2_P3_GaussianGenerativeModel.py
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,12 +12,6 @@ from sklearn.preprocessing import StandardScaler
 eta = 0.001 # Learning rate
 lam = 0.0001 # Lambda for regularization
 
-# Whether or not you want the plots to be displayed
-show_charts = True
-
-# -----------------------------------------------------------------
-
-# Visualize the decision boundary that a model produces
 def visualize_boundary(model, X, y, title, xlabel, ylabel, imgname, show, width=2):
     # Create a grid of points
     if show:
@@ -34,8 +25,7 @@ def visualize_boundary(model, X, y, title, xlabel, ylabel, imgname, show, width=
                 ['Developed', 'Developing'], 
                 framealpha=0.3, scatterpoints=1)
         plt.show()
-        plt.savefig(imgname)
-        print("Accuracy: ", model.score(x_s, y_s))
+        print("Accuracy for " + imgname + ": ", model.score(x_s, y_s))
 
 
 data = pd.read_excel(r"./math_proj.xlsx")
@@ -48,7 +38,7 @@ visualize_boundary(lr, x_s, y_s,
                     "Using Logistic Regression to Predict Whether a Nation is Developed or Developing\n Based on GDP Growth and Human Development Index", 
                     "Real GDP Growth",
                     "Human Development Index",
-                    'gdp_vs_hdi', False)
+                    'gdp_vs_hdi', True)
 
 data = pd.read_excel(r"./math_proj_2.xlsx")
 x1 = data["Inflation (% change) "].to_numpy()
@@ -58,21 +48,19 @@ x_s = np.concatenate((x1.reshape(x1.shape[0],1), x2.reshape(x2.shape[0],1)), axi
 scaler.fit(x_s)
 x_s = scaler.transform(x_s)
 y_s = data["Class"].to_numpy()
-lr = LogisticRegression(eta=eta, lam=lam, runs=5000)
+lr = LogisticRegression(eta=eta, lam=lam, runs=5000, basis="quad")
 visualize_boundary(lr, x_s, y_s, 
                     "Using Logistic Regression to Predict Whether a Nation is Developed or Developing\n Based on Inflation and Government Expenditure", 
-                    "Inflation (% change)",
-                    "General Government Total Expenditure (%GDP)",
-                    'inflation_vs_expenditure', False)
+                    "Inflation (% change) (Standardized)",
+                    "General Government Total Expenditure (%GDP) (Standardized)",
+                    'inflation_vs_expenditure', True)
 
 data = pd.read_excel(r"./math_proj_2.xlsx")
 x1 = data["Inflation (% change) "].to_numpy()
 x2 = data["General government net lending/borrowing (% GDP)"].to_numpy()
-scaler = StandardScaler()
 x_s = np.concatenate((x1.reshape(x1.shape[0],1), x2.reshape(x2.shape[0],1)), axis=1)
-scaler.fit(x_s)
 y_s = data["Class"].to_numpy()
-lr = LogisticRegression(eta=eta, lam=lam, runs=5000)
+lr = LogisticRegression(eta=eta, lam=lam, runs=5000, basis="doublequad")
 visualize_boundary(lr, x_s, y_s, 
                     "Using Logistic Regression to Predict Whether a Nation is Developed or Developing\n Based on Inflation and Government Net Lending and Borrowing", 
                     "Inflation (% change)",
@@ -92,9 +80,8 @@ visualize_boundary(lr, x_s, y_s,
                     "Using Logistic Regression to Predict Whether a Nation is Developed or Developing\n Based on Duration of Paid Maternity Leave and Government Expenditure", 
                     "Duration of Paid Maternity Leave (Standardized)",
                     "Government Expenditure (Standardized)",
-                    'maternity_vs_expenditure', False)
+                    'maternity_vs_expenditure', True)
 
-# change these parameters
 data = pd.read_excel(r"./math_proj_4.xlsx")
 x1 = data["Tech"].to_numpy()
 x2 = data["Births"].to_numpy()
@@ -108,4 +95,4 @@ visualize_boundary(lr, x_s, y_s,
                     "Using Logistic Regression to Predict Whether a Nation is Developed or Developing\n Based on Share of Female STEM Graduates and Proportion of Births Attended by Healthcare Professionals", 
                     "Share of Female STEM Graduates (Standardized)",
                     "Proportion of Births Attended by Healthcare Professionals (Standardized)",
-                    'tech_vs_births', False)
+                    'tech_vs_births', True)
--- logistic_regression.py
+++ logistic_regression.py
@@ -4,33 +4,37 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 class LogisticRegression:
-    def __init__(self, eta, lam, runs):
+    def __init__(self, eta, lam, runs, basis="lin"):
         self.eta = eta
         self.lam = lam
         self.runs = runs
+        self.basis = basis
 
     def fit(self, X, y):
-        # for government expenditure
-        # self.X = np.c_[np.ones(len(X)), X, X[:,0]**2]
-        # for government borrowing
-        self.X = np.c_[np.ones(len(X)), X, X[:,0]**2, X[:,1]**2]
-        #regular
-        # self.X = np.c_[np.ones(len(X)), X]
+        # adds another column to each datapoint x_n that is the first entry in x_n squared 
+        # (used for inflation vs government expenditure dataset)
+        if self.basis == "quad": self.X = np.c_[np.ones(len(X)), X, X[:,0]**2]
+        # adds two more columns to each datapoint x_n that are both entires in x_n squared 
+        # (used for inflation vs government borrowing)
+        elif self.basis == "doublequad": self.X = np.c_[np.ones(len(X)), X, X[:,0]**2, X[:,1]**2]
+        # no basis transformation
+        else: self.X = np.c_[np.ones(len(X)), X]
+
         self.len, self.feat = self.X.shape
         self.y_target = y
         self.W = np.random.rand(self.feat)
         self.loss = []
-
         self.num_runs = 0
 
-
+        # this implementation of gradient descent runs a fixed number of times, but an
+        # epsilon-based approach may also be used
         while self.num_runs < self.runs:
-            gradient_wrt_i = 0
+            gradient = 0
             for i in range(self.len):
                 z = np.dot(self.W, self.X[i])
-                gradient_wrt_i += (self.__sigmoid(z) - self.y_target[i]) * self.X[i]
-            gradient_wrt_i += self.lam * self.W
-            self.W -= self.eta * gradient_wrt_i
+                gradient += (self.__sigmoid(z) - self.y_target[i]) * self.X[i]
+            gradient += self.lam * self.W
+            self.W -= self.eta * gradient
             self.loss.append(self.__calculate_loss())
             self.num_runs += 1
 
@@ -46,14 +50,19 @@ class LogisticRegression:
             loss = loss + (self.y_target[i])*(np.log(self.__sigmoid(z))) + (1-self.y_target[i])*(np.log(1-self.__sigmoid(z)))
         return -loss + self.lam/2 * np.linalg.norm(self.W)**2
 
+    # uses the values of parameters w calculated in the fit() function to predict the class
+    # of each point x in the matrix X_pred
     def predict(self, X_pred):
         preds = []
         # for government expenditure
-        # X_pred = np.c_[np.ones(len(X_pred)), X_pred, X_pred[:,0]**2]
+        if self.basis == "quad":
+            X_pred = np.c_[np.ones(len(X_pred)), X_pred, X_pred[:,0]**2]
         # for government borrowing
-        X_pred = np.c_[np.ones(len(X_pred)), X_pred, X_pred[:,0]**2, X_pred[:,1]**2]
+        elif self.basis == "doublequad":
+            X_pred = np.c_[np.ones(len(X_pred)), X_pred, X_pred[:,0]**2, X_pred[:,1]**2]
         # regular
-        # X_pred = np.c_[np.ones(len(X_pred)), X_pred]
+        else:
+            X_pred = np.c_[np.ones(len(X_pred)), X_pred]  
         for i in range(len(X_pred)):
             z = np.dot(self.W, X_pred[i])
             sigmoid = self.__sigmoid(z)
@@ -61,22 +70,11 @@ class LogisticRegression:
             else: preds.append(0)
         return np.array(preds)
     
+    # accuracy
     def score(self, X, y_true):
         return np.count_nonzero((self.predict(X) - y_true) == 0)/len(y_true)
     
+    # plots the value of the loss function at every run
     def plot_loss(self):
         plt.plot(np.linspace(0, self.num_runs, self.num_runs), self.loss)
         plt.show()
-
-# data = pd.read_excel(r"./math_proj_3.xlsx")
-# x1 = data["Maternity"].to_numpy()
-# x2 = data["Government Expenditure"].to_numpy()
-# scaler = StandardScaler()
-# x_s = np.concatenate((x1.reshape(x1.shape[0],1), x2.reshape(x2.shape[0],1)), axis=1)
-# scaler.fit(x_s)
-# x_s = scaler.transform(x_s)
-# y_s = data["Class"].to_numpy()
-# lr = LogisticRegression(eta=0.001, lam=0.00, runs=1000)
-# lr.fit(x_s, y_s)
-# print(lr.score(x_s, y_s))
-# lr.plot_loss()
